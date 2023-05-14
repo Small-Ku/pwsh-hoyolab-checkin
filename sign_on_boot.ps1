5aada4d2ddb9f62980d75981a02c6812a8419a92
Small_Ku <small_ku@outlook.com>
2023-05-14 21:55:18 +0800

    refactor: rename

--- sign_on_boot.ps1
+++ sign_on_boot.ps1
@@ -1,5 +1,5 @@
 $objShell = New-Object -ComObject ("WScript.Shell")
-$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"+"\pwsh-genshin-sign.lnk")
+$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"+"\pwsh-hoyolab-checkin.lnk")
 $objShortCut.TargetPath = (Get-Command "powerShell.exe").Source
 $objShortCut.Arguments = "-WindowStyle Hidden $((Get-Item .\sign.ps1 | Resolve-Path).ProviderPath)"
 $objShortCut.WorkingDirectory = "$(((Get-Item .\sign.ps1).Directory | Resolve-Path).ProviderPath)"
